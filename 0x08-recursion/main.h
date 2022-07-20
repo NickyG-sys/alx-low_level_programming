fa5c59b9a641c66e0225e59513d3eae991d02941
NickyG-sys <52282761+NickyG-sys@users.noreply.github.com>
2022-07-20 15:24:09 +0200

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,3 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
 int _putchar(char c)
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -10,5 +8,3 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
-#endif
